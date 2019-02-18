c8be188d1f0bf566b723f8ca68498c09a7c86d54
MichellMonroy <43971609+MichellMonroy@users.noreply.github.com>
2019-02-17 21:44:48 -0600

    Add files via upload

---
+++ test.py
@@ -0,0 +1,23 @@
+def inc(x):
+    return x + 1
+
+def test_answer():
+    assert inc(8) == 6
+
+#import pytest
+#def f():
+    #raise SystemExit(1)
+
+#def test_mytest():
+    #with pytest.raises(SystemExit):
+        #f()
+        
+# content of test_class.py
+#class TestClass(object):
+    #def test_one(self):
+        #x = "this"
+        #assert 'h' in x
+
+    #def test_two(self):
+        #x = "hello"
+        #assert hasattr(x, 'check')
